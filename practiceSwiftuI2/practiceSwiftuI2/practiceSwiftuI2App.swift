7567b8b72f04445a5f5010d064a7f2232005eeec
GeonH0 <h2jo369@naver.com>
2023-08-03 22:05:00 +0900

    set practice

---
+++ practiceSwiftuI2/practiceSwiftuI2/practiceSwiftuI2App.swift
@@ -0,0 +1,17 @@
+//
+//  practiceSwiftuI2App.swift
+//  practiceSwiftuI2
+//
+//  Created by 김건호 on 2023/08/03.
+//
+
+import SwiftUI
+
+@main
+struct practiceSwiftuI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
